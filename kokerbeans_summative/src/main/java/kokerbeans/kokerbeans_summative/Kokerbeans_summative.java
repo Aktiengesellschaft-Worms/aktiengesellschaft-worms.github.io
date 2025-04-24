e706826bc50264aaedf7f606beebcc26d86c325b
gsorg1 <gsorg1@WCAR-CMT-205964.ad.ocdsb.ca>
2025-04-24 08:42:36 -0400

    1st html test

---
+++ kokerbeans_summative/src/main/java/kokerbeans/kokerbeans_summative/Kokerbeans_summative.java
@@ -0,0 +1,9 @@
+
+package kokerbeans.kokerbeans_summative;
+
+public class Kokerbeans_summative {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
