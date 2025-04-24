66ada57fb12cafef6bd78742324917da626245a2
monekenke <73973148+monekenke@users.noreply.github.com>
2025-04-24 08:56:18 -0400

    Delete kokerbeans_summative directory

--- kokerbeans_summative/src/main/java/kokerbeans/kokerbeans_summative/Kokerbeans_summative.java
+++
@@ -1,9 +0,0 @@
-
-package kokerbeans.kokerbeans_summative;
-
-public class Kokerbeans_summative {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
